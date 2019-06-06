f549bafa99adb8927c5a19072fcb320dd42deb6a
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-06-05 18:19:07 -0700

    worked through almost the entirety of my project. got really into it and forgot to commit as much. buttons are appending correctly. the value of the user input is being pushed to the array which creates an additional button. giphy API working great and is only displaying 10 images. need to work on data-still and data-animate.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,48 +1,87 @@
+//buttons already on the page when the page loaded  ----x
+        //JS is populating the buttons in the top ----x
+//when you click on the button:
+      //10 images are displayed from the giphy API of the topic
+      // these images are responsive in terms of clicking on them to start and stop
+      // each button is part of an array
+              // the array is looping through and generating a button for each one
+              //the button's have to have or name or whatever that is linked to the giphy API
+              // on click 
+
+//there will an input that will have to store the users answers as a value
+        //this will then have to be created into a button that also acceses the giphy api
+
+// $(document).ready(function(){
 
 var topics = ["eggs benedict" , "green eggs & ham" , "pancakes" , "chicken fried steak" , "cornbeef hash" , "biscuits & gravy"];
 
-$(".gif").on("click", function() {
+function renderButtons() {
 
-    var state = $(this).attr("data-state");
+  $("#button-holder").empty();
 
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-    }
+  for (i = 0; i < topics.length; i++) {
 
-    var breakfast = $(this).attr("data-breakfast");
+    var button = $("<button>");
+    button.addClass("button");
+    button.attr("data-name" , topics[i]);
+    button.text(topics[i]);
+    $("#button-holder").append(button);
+  }
 
-  //add breakfast data attribute to gifs
+$(".button").on("click" , function(){
 
-  //add state to gips
-
-      
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        breakfast + "&api_key=MWcpJfnOXM8K47gFSCBq3kOPopsVa2Cp";
+    var breakfast = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + breakfast + "&api_key=MWcpJfnOXM8K47gFSCBq3kOPopsVa2Cp&limit=10";
 
       $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-          console.log(queryURL);
 
-          console.log(response);
-          var results = response.data;
+      console.log(response);
 
-          for (var i = 0; i < results.length; i++) {
+        var results = response.data;
 
-            var breakfastDiv = $("<div>");
+        for (i = 0; i < results.length; i++) {
 
-            var breakfastImage = $("<img>");
+          var gifDiv = $("<div>")
+          var gif = $("<img>");
 
-            breakfastImage.attr("src", results[i].images.fixed_height.url);
+          gif.attr("src" , results[i].images.fixed_height.url);
 
-            breakfastDiv.append(animalImage);
+          gifDiv.append(gif);
 
-            $("#breakfast-gifs-here").prepend(breakfastDiv);
-          }
+          $("#breakfast-gifs-here").prepend(gifDiv);
       }
     })
+  })
+
+}
+
+// })
+
+$("#giferate").on("click" , function(){
+
+  event.preventDefault();
+
+  var giferate = $("#breakfast-input").val().trim();
+
+  topics.push(giferate);
+
+  renderButtons();
+});
+
+renderButtons();
+
+
+// $(".gif").on("click", function() {
+
+//   var state = $(this).attr("data-state");
+
+//   if (state === "still") {
+//     $(this).attr("src", $(this).attr("data-animate"));
+//     $(this).attr("data-state", "animate");
+//   } else {
+//     $(this).attr("src", $(this).attr("data-still"));
+//     $(this).attr("data-state", "still");
+//   }
